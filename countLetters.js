4459a2838000fc4f584d3779484fc9aca34d39fe
dahir yusuf <dahir.yusuf@hotmail.com>
2021-10-27 23:05:20 +0000

    Counting through a sentence

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const results = {};
+
+  for (const item of sentence) {
+    if (item !== ' ') {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+  return results;
+}
+
