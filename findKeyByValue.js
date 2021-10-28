e29359137fcbfcb3b66b41d2bc1bea7dde4deec7
dahir yusuf <dahir.yusuf@hotmail.com>
2021-10-28 01:14:05 +0000

    finding a key

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, input) {
+  let key;
+  let i = 0;
+  for (const item in obj) {
+    let val = obj[item];
+    if (val === input) {
+      key = item;
+    }
+  }
+  return key;
+}
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+findKeyByValue(bestTVShowsByGenre, "The Wire");
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
