bc0f6f7365a8454dad0884acbf1e7ab14104a42f
dahir yusuf <dahir.yusuf@hotmail.com>
2021-10-28 00:37:11 +0000

    letterpositions

---
+++ letterPositions.js
@@ -0,0 +1,43 @@
+const eqArrays = function(arr, arr2) {
+  let x = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr2[i]) {
+      x++
+    }
+  }
+  if (x === arr.length) {
+    return true;
+  }
+  else {
+    return false;
+
+  }
+}
+
+const assertArrayEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const countLetters = function(sentence) {
+  const results = {};
+  let i = 0;
+  for (const item of sentence) {
+    if (item !== ' ') {
+      if (results[item]) {
+        results[item].push(i);
+      } else {
+        results[item] = [i];
+      }
+    }
+    i++;
+  }
+  return results;
+}
+
+countLetters("hello")
+assertArrayEqual(countLetters("hello").e, [1])
