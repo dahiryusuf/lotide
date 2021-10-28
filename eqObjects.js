4f9fbc3730e2fb7a45e3ba3ce356ede5f1748e25
dahir yusuf <dahir.yusuf@hotmail.com>
2021-10-28 14:48:40 +0000

    final code

--- eqObjects.js
+++ eqObjects.js
@@ -11,24 +11,30 @@ const assertEqual = function(actual, expected) {
 const eqObjects = function(object1, object2) {
 //if object ones values = object2s values but it should loop through object 2 then compare each value to each value in object1
 let x = 0;
-if(object1.length === object2.length){
+if(Object.keys(object1).length === Object.keys(object2).length){
   for(let val in object1){
     for(let i in object2){
       if(val === i && object1[val] === object2[i]){
         x++;
-        console.log(x)
       }
     }
   }
 }
+  if(x === Object.keys(object1).length){
+    return true;
+  }
+  else{
+    return false;
+  }
+
 };
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 eqObjects(ab, ba); // => true
 
-//const abc = { a: "1", b: "2", c: "3" };
-//eqObjects(ab, abc);
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc);
 
-//assertEqual(eqObjects(ab, ba), true);
-//assertEqual(eqObjects(ab, abc), false);
+assertEqual(eqObjects(ab, ba), true);
+assertEqual(eqObjects(ab, abc), false);
