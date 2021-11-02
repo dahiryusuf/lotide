b9f8eae9dff7cd12e714e50ad93aab62c087a018
dahir yusuf <dahir.yusuf@hotmail.com>
2021-11-02 18:16:41 +0000

    tests and refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr, arr2){
   let x = 0;
   for(let i = 0;i < arr.length;i++){
@@ -21,6 +13,5 @@ const eqArrays = function(arr, arr2){
     
   }
 }
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1])
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+module.exports = eqArrays;
